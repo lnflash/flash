997ec714e332b63203dd46e8974de35ed32a3d0b
brh28 <31115595+brh28@users.noreply.github.com>
2024-02-14 11:18:12 -0600

    Fix logging for webhook (#37)
    * Fix logging for webhook
    
    * websocket listener

---
+++ src/debug/websocket/subscriptions/price.js
@@ -0,0 +1,21 @@
+
+
+// const SUBSCRIPTION_QUERY = gql`
+// subscription Subscription($input: LnInvoicePaymentStatusInput!) {
+//   	lnInvoicePaymentStatus(input: $input) {
+//     		errors {
+//       		message
+//     	}
+//     		status
+//   	}
+// }
+// `;
+
+// export const priceUpdatesGql = (pr) => ({
+//     query: SUBSCRIPTION_QUERY,
+//     variables: {
+//         input: {
+//             "paymentRequest": pr
+//         }
+//     }
+// })
