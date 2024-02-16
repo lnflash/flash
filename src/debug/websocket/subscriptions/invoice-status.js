172234049b2dcda93bfe1f17ff59db5c8ee5b9d3
brh28 <31115595+brh28@users.noreply.github.com>
2024-02-16 15:44:51 -0600

    Trigger Account update events from Ibex transactions (#42)
    * call notification service lightningTxReceive
    
    * Use galoy ln trx receive for notifications and get context data from mongo
    
    * Remove unused logs

--- src/debug/websocket/subscriptions/invoice-status.js
+++ src/debug/websocket/subscriptions/invoice-status.js
@@ -1,23 +1,39 @@
 const { gql } = require('@apollo/client');
 
+//const SUBSCRIPTION_QUERY = gql`
+// subscription Subscription($input: LnInvoicePaymentStatusInput!) {
+//   	lnInvoicePaymentStatus(input: $input) {
+//     		errors {
+//       		message
+//     	}
+//     		status
+//   	}
+// }
+// `;
+
 const SUBSCRIPTION_QUERY = gql`
-subscription Subscription($input: LnInvoicePaymentStatusInput!) {
-  	lnInvoicePaymentStatus(input: $input) {
-    		errors {
-      		message
-    	}
-    		status
-  	}
-}
+    subscription myLnUpdates {
+        myUpdates {
+            errors {
+                message
+            }
+            update {
+                ... on LnUpdate {
+                    paymentHash
+                    status
+                }
+            }
+        }
+    }
 `;
 
 module.exports = {
-    invoiceStatusGql: (pr) => ({
+    invoiceStatusGql: () => ({
         query: SUBSCRIPTION_QUERY,
-        variables: {
-            input: {
-                "paymentRequest": pr
-            }
-        }
+        // variables: {
+        //     input: {
+        //         "paymentRequest": pr
+        //     }
+        // }
     })
 }
