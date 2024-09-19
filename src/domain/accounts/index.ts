987fa49b716fb244632273ce9cf4b4357e239d03
Dread <34528298+islandbitcoin@users.noreply.github.com>
2024-09-19 16:56:17 -0500

    modified Username regex

--- src/domain/accounts/index.ts
+++ src/domain/accounts/index.ts
@@ -67,7 +67,8 @@ export const checkedAccountStatus = (status: string) => {
   return status as AccountStatus
 }
 
-export const UsernameRegex = /(?!^(1|3|bc1|lnbc1))^[0-9a-z_]{3,50}$/i
+// export const UsernameRegex = /(?!^(1|3|bc1|lnbc1))^[0-9a-z_]{3,50}$/i
+export const UsernameRegex = /^[\p{L}0-9_]+$/u
 
 export const checkedToUsername = (username: string): Username | ValidationError => {
   if (!username.match(UsernameRegex)) {
