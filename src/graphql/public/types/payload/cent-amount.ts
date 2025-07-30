f548b991cb17180b62ba4a1267252e05075bdaaf
Benjamin Hindman <bhindman@proton.me>
2025-07-30 08:43:58 -0500

    Update fee response to maintain backward compatibility

--- src/graphql/public/types/payload/cent-amount.ts
+++ src/graphql/public/types/payload/cent-amount.ts
@@ -2,6 +2,7 @@ import { GT } from "@graphql/index"
 
 import IError from "@graphql/shared/types/abstract/error"
 import CentAmount from "@graphql/public/types/scalar/cent-amount"
+import USDCentsScalar from "@graphql/shared/types/scalar/usd-cents"
 
 const CentAmountPayload = GT.Object({
   name: "CentAmountPayload",
@@ -9,8 +10,12 @@ const CentAmountPayload = GT.Object({
     errors: {
       type: GT.NonNullList(IError),
     },
+    // the fee amount of the invoice 
     amount: {
-      type: CentAmount,
+      type: USDCentsScalar,
+    },
+    invoiceAmount: {
+      type: USDCentsScalar,
     },
   }),
 })
