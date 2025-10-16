0f9f6cb14d9a71fc962fd513d4a89c1de5cdec58
Dread <34528298+islandbitcoin@users.noreply.github.com>
2025-10-16 17:21:01 -0400

    refactored to account for ERPNext integration

--- src/services/frappe/errors.ts
+++ src/services/frappe/errors.ts
@@ -5,3 +5,5 @@ export class JournalEntryDraftError extends ErpNextError {}
 export class JournalEntryTitleError extends JournalEntryDraftError {}
 export class JournalEntrySubmitError extends ErpNextError {}
 export class JournalEntryDeleteError extends ErpNextError {}
+export class IssueCreateError extends ErpNextError {}
+export class IssueQueryError extends ErpNextError {}
