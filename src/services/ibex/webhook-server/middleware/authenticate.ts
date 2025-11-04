8d08237bf8242ce5efd1d85a28e910aba62cb21f
Abhay <toabhayraizada@gmail.com>
2025-11-04 20:49:35 +0530

    Remove log

--- src/services/ibex/webhook-server/middleware/authenticate.ts
+++ src/services/ibex/webhook-server/middleware/authenticate.ts
@@ -2,7 +2,7 @@ import { Request, Response, NextFunction } from "express"
 import { IbexConfig } from "@config"
 
 export const authenticate = (req: Request, resp: Response, next: NextFunction) => {
-  console.log("Authenticating webhook request", req.body.webhookSecret, IbexConfig.webhook.secret)
-  if (req.body.webhookSecret !== IbexConfig.webhook.secret) return resp.status(401).end("Invalid secret")
-  next();
-};
+  if (req.body.webhookSecret !== IbexConfig.webhook.secret)
+    return resp.status(401).end("Invalid secret")
+  next()
+}
