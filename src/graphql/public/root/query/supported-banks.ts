2e74b479bfda143b6c9b419a771f28fbf86b2b57
Benjamin Hindman <bhindman@proton.me>
2026-02-12 13:17:16 -0600

    Map BankQueryError to gql type

--- src/graphql/public/root/query/supported-banks.ts
+++ src/graphql/public/root/query/supported-banks.ts
@@ -1,15 +1,17 @@
 import { Accounts } from "@app"
 
 import { GT } from "@graphql/index"
-import { mapError } from "@graphql/error-map"
+import { mapAndParseErrorForGqlResponse, mapError } from "@graphql/error-map"
 import Bank from "@graphql/public/types/object/bank"
+import { BanksQueryError } from "@services/frappe/errors"
+import { InternalServerError } from "@graphql/error"
+import { baseLogger } from "@services/logger"
 
 const SupportedBanksQuery = GT.Field({
   type: GT.NonNullList(Bank),
   resolve: async () => {
     const banks = await Accounts.getSupportedBanks()
-    if (banks instanceof Error) throw mapError(banks)
-
+    if (banks instanceof BanksQueryError) return new InternalServerError({ logger: baseLogger })
     return banks
   },
 })
