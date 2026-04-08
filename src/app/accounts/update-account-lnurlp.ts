8f8c00882ede704eba04aad5ef19e000bccc0058
heyolaniran <olaniran.abd@gmail.com>
2026-04-08 10:25:13 +0100

    Add and update lnurlp
    feat: add lnurlps field to account schema and make update lnurlp mutation

---
+++ src/app/accounts/update-account-lnurlp.ts
@@ -0,0 +1,54 @@
+
+import { UnexpectedIbexResponse } from "@services/ibex/errors"
+import ibex from "@services/ibex"
+import { AccountsRepository, WalletsRepository } from "@services/mongoose"
+
+export const updateLnurlp = async ({
+    accountId,
+    lnurlp,
+    walletId
+}: { accountId: AccountId, lnurlp?: string, walletId?: WalletId }): Promise<Account | ApplicationError> => {
+
+    let lnurlpToUpdate: string
+    const account = await AccountsRepository().findById(accountId);
+
+    if (account instanceof Error) return account
+
+    const targetWalletId = walletId || account.defaultWalletId;
+
+    if (lnurlp) {
+        lnurlpToUpdate = lnurlp
+    } else {
+
+        const wallet = await WalletsRepository().findById(targetWalletId)
+
+        if (wallet instanceof Error) return wallet
+
+        const lnurlpResp = await ibex.client.createLnurlPay({
+            accountId: targetWalletId as string,
+        })
+
+        if (lnurlpResp instanceof Error) return lnurlpResp
+        if (!lnurlpResp.lnurl) return new UnexpectedIbexResponse("Failed to create lnurlp")
+
+        lnurlpToUpdate = lnurlpResp.lnurl
+    }
+
+    const lnurlps = account.lnurlps || []
+
+    const updatedLnurlps = lnurlps.map((item) => ({
+        lnurlp: item.lnurlp,
+        active: false,
+        walletId: item.walletId
+    }))
+
+    updatedLnurlps.push({
+        lnurlp: lnurlpToUpdate,
+        active: true,
+        walletId: targetWalletId || account.defaultWalletId
+    })
+
+    account.lnurlps = updatedLnurlps
+
+    return AccountsRepository().update(account)
+}
